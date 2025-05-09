e5a2ef34a7a63a0efc30e0af9b481249b621a1a1
menintel <menintel@tuta.io>
2025-05-09 19:19:39 +0100

    created book appointment

--- base/views.py
+++ base/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from base import models as base_models
+from doctor import models as doctor_models
+from patient import models as patient_models
+
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -16,3 +21,57 @@ def service_detail(request, service_id):
     }
     return render(request, "base/service_detail.html", context)
 
+
+@login_required
+def book_appointment(request, service_id, doctor_id):
+
+    service = base_models.Service.objects.get(id=service_id)
+    doctor = doctor_models.Doctor.objects.get(id=doctor_id)
+    patient =  patient_models.Patient.objects.get(user=request.user)
+
+    if request.method == "POST":
+        full_name = request.POST.get("full_name")
+        email = request.POST.get("email")
+        mobile = request.POST.get("mobile")
+        gender = request.POST.get("gender")
+        address = request.POST.get("address")
+        dob = request.POST.get("dob")
+        issues = request.POST.get("issues")
+        symptoms = request.POST.get("symptoms")
+
+        #Update Patient bio data
+        patient.full_name = full_name
+        patient.email = email
+        patient.dob = dob
+        patient.mobile = mobile
+        patient.gender = gender
+        patient.address = address
+        patient.save()
+        
+        # Create appointement Object
+        appointment = base_models.Appointment.objects.create (
+            service = service,
+            doctor = doctor,
+            patient = patient,
+            appointment_date = doctor.next_available_appointment_date,
+            issues = issues,
+            symptoms = symptoms,
+        )
+
+        billing = base_models.Billing()
+        billing.patient = patient
+        billing.appointment = appointment
+        billing.sub_total = appointment.service.cost
+        billing.tax = appointment.service.cost * 5 / 100
+        billing.total = billing.sub_total + billing.tax
+        billing.status = "Unpaid"
+        
+        return redirect("base:checkout", billing.billing_id)
+
+    context = {
+        "service":service,
+        "doctor":doctor,
+        "patient":patient,
+    }
+
+    return render(request, "base/book_appointment.html", context)
