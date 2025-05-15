5fe892944b8d926b7faffd4d3e580107d16b4649
menintel <menintel@tuta.io>
2025-05-15 23:54:25 +0100

    completed appointment details, added alert and  cancel, activate and complete appointment

--- doctor/views.py
+++ doctor/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from doctor import models as doctor_models
 from base import models as base_models
 
@@ -15,3 +16,67 @@ def dashboard(request):
         'appointments': appointments,
         'notifications': notifications, 
     }
+
+    return render(request, 'doctor/dashboard.html', context)
+
+@login_required
+def appointments(request):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointments = base_models.Appointment.objects.filter(doctor=doctor)
+
+    context  = {
+        'appointments': appointments, 
+    }
+
+    return render(request, 'doctor/appointments.html', context)
+
+@login_required
+def appointment_detail(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    medical_records = base_models.MedicalRecord.objects.filter(appointment=appointment)
+    lab_tests = base_models.LabTest.objects.filter(appointment=appointment)
+    prescriptions = base_models.Prescription.objects.filter(appointment=appointment)
+
+    context = {
+        'appointment': appointment,
+        'medical_records': medical_records,
+        'lab_tests': lab_tests,
+        'prescriptions': prescriptions,
+    }
+
+    return render(request, 'doctor/appointment_detail.html', context)
+
+@login_required
+def cancel_appointment(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    appointment.status = 'Cancelled'
+    appointment.save()
+
+    messages.success(request, 'Appointment cancelled successfully.')
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+@login_required
+def activate_appointment(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    appointment.status = 'Scheduled'
+    appointment.save()
+
+    messages.success(request, 'Appointment Re-Scheduled successfully.')
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
+
+@login_required
+def complete_appointment(request, appointment_id):
+    doctor = doctor_models.Doctor.objects.get(user=request.user)
+    appointment = base_models.Appointment.objects.get(appointment_id=appointment_id, doctor=doctor)
+
+    appointment.status = 'Completed'
+    appointment.save()
+
+    messages.success(request, 'Appointment Completed successfully.')
+    return redirect("doctor:appointment_detail", appointment.appointment_id)
